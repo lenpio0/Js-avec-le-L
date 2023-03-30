726c1074ef07a13316ca6fb77a6dc314dfa2a328
Louis-OFP <louis170304@gmail.com>
2023-03-30 09:52:53 +0200

    uptade en bien

--- Citations/script.js
+++ Citations/script.js
@@ -4,29 +4,39 @@ let citation = document.getElementById("citation");
 button.onclick = btn;
 
 function btn() {
-    let quote = Math.floor(Math.random() * 7);
+    let quote = Math.floor(Math.random() * 8);
     switch (quote){
         case 0:
-            citation.innerHTML = "« Ta mère c'est de l'or, ne la fais pas pleurer pour de l'argent. » Le J"
+            citation.innerHTML = "« Ta mère c'est de l'or, ne la fais pas pleurer pour de l'argent. » "
+            auteur.innerHTML = "Le J"
             break;
         case 1:
-            citation.innerHTML = "« Nage droit devant toi, nage droit devant toi » Dory "
+            citation.innerHTML = "« Nage droit devant toi, nage droit devant toi » "
+            auteur.innerHTML = "Dory "
             break;
         case 2:
-            citation.innerHTML = "« Eat some chocolate chocolate chip» Joe Biden"
+            citation.innerHTML = "« Eat some chocolate chocolate chip» "
+            auteur.innerHTML = "Joe Biden"
             break;
         case 3:
-            citation.innerHTML = "« Yo tout le monde c'est Squeezie» Squeezie"
+            citation.innerHTML = "« Yo tout le monde c'est Squeezie» "
+            auteur.innerHTML = "Squeezie"
             break;
         case 4:
-            citation.innerHTML = "«Rien n'est plus semblable à l'identique que ce qui est pareil à la même chose» Pierre Dac"
+            citation.innerHTML = "«Rien n'est plus semblable à l'identique que ce qui est pareil à la même chose» "
+            auteur.innerHTML = "Pierre Dac"
             break;
         case 5:
-            citation.innerHTML = " «T'es sérieux toi ?» Clément Da Silva"
+            citation.innerHTML = " «T'es sérieux toi ?» "
+            auteur.innerHTML = "Clément de Sousa"
             break;
         case 6:
             citation.innerHTML = "«Ne vois pas ça comme un gros mur, mais comme un escalier.»"
             auteur.innerHTML = "L'autre Clément"
+            break;
+        case 7:
+            citation.innerHTML = "«Le J c'est le S et rien qu'on fait du JS»"
+            auteur.innerHTML = "Champion anonyme"
             break;
         }
     }
