ec27157ad32a4986e5a279026941b9e4b78fe933
lenpio0 <127386860+lenpio0@users.noreply.github.com>
2023-04-03 11:04:12 +0200

    trucs

--- Memory/script.js
+++ Memory/script.js
@@ -6,6 +6,9 @@ let carte5 = document.getElementById('carte5');
 let carte6 = document.getElementById('carte6');
 let carte7 = document.getElementById('carte7');
 let carte8 = document.getElementById('carte8');
+let startBtn = document.getElementById('start');
+let wall = document.getElementById('wall');
+let win = document.getElementById('win');
 
 let pioche1 = 0;
 let pioche2 = 0;
@@ -19,6 +22,39 @@ carte5.onclick = card5;
 carte6.onclick = card6;
 carte7.onclick = card7;
 carte8.onclick = card8;
+startBtn.onclick = start;
+
+let cartes = [carte1, carte2, carte3, carte4, carte5, carte6, carte7, carte8];
+
+function start() {
+    startBtn.classList.add('hidden');
+    carte1.classList.remove('hidden');
+    carte2.classList.remove('hidden');
+    carte3.classList.remove('hidden');
+    carte4.classList.remove('hidden');
+    carte5.classList.remove('hidden');
+    carte6.classList.remove('hidden');
+    carte7.classList.remove('hidden');
+    carte8.classList.remove('hidden');
+    // randomOrder(); 
+    check = [];
+    cartes.forEach(function(carte) {
+        let order = Math.floor(Math.random() * 8);
+        if (check.includes(order) == false) {
+            check.push(order);
+            carte.classList.add('order-' + order);
+        }
+        else {
+             do {
+                order = Math.floor(Math.random() * 8);
+                if (check.includes(order) == false) {
+                    check.push(order);
+                    carte.classList.add('order-' + order);
+                }      
+            }while (check.includes(order))
+        }
+    })   
+}
 
 function restart() {
     carte1.onclick = card1;
@@ -31,6 +67,29 @@ function restart() {
     carte8.onclick = card8;
     pioche1 = 0;
     pioche2 = 0;
+    startBtn.classList.remove('hidden');
+    carte1.classList.add('hidden', 'bg-none');
+    carte2.classList.add('hidden', 'bg-none');
+    carte3.classList.add('hidden', 'bg-none');
+    carte4.classList.add('hidden', 'bg-none');
+    carte5.classList.add('hidden', 'bg-none');
+    carte6.classList.add('hidden', 'bg-none');
+    carte7.classList.add('hidden', 'bg-none');
+    carte8.classList.add('hidden', 'bg-none');
+    carte1.classList.replace('border-green-600', 'border-black');
+    carte2.classList.replace('border-green-600', 'border-black');
+    carte3.classList.replace('border-green-600', 'border-black');
+    carte4.classList.replace('border-green-600', 'border-black');
+    carte5.classList.replace('border-green-600', 'border-black');
+    carte6.classList.replace('border-green-600', 'border-black');
+    carte7.classList.replace('border-green-600', 'border-black');
+    carte8.classList.replace('border-green-600', 'border-black');
+    win.classList.add('hidden');
+}
+
+function randomOrder() {
+
+    
 }
 
 function resetCard1() {
@@ -65,17 +124,110 @@ function resetCard8() {
     carte8.onclick = card8;
 }
 
+function nextTurn() {
+    wall.classList.remove('hidden');
+    setTimeout(function() {
+        wall.classList.add('hidden');
+    }, 600)
+    if (carte1.onclick == null) {
+        carte1.classList.remove("border-red-600");
+        carte1.classList.add("border-green-600");
+    }
+    else if (carte1.onclick != null) {
+        setTimeout(function() {
+            carte1.classList.remove("border-red-600", "bg-[url(images/spidermacron.webp)]");
+            carte1.classList.add("border-black", "bg-none");
+        }, 600);
+    }    
+    if (carte2.onclick == null) {
+        carte2.classList.remove("border-red-600");
+        carte2.classList.add("border-green-600");
+    }
+    else if (carte2.onclick != null) {
+        setTimeout(function() {
+            carte2.classList.remove("border-red-600", "bg-[url(images/spidermacron.webp)]");
+            carte2.classList.add("border-black", "bg-none");
+        }, 600);
+    }
+    if (carte3.onclick == null) {
+        carte3.classList.remove("border-red-600");
+        carte3.classList.add("border-green-600");
+    }
+    else if (carte3.onclick != null) {
+        setTimeout(function() {
+            carte3.classList.remove("border-red-600", "bg-[url(images/drake_en_mode_donuts.webp)]");
+            carte3.classList.add("border-black", "bg-none");
+        }, 600);
+    }
+    if (carte4.onclick == null) {
+        carte4.classList.remove("border-red-600");
+        carte4.classList.add("border-green-600");
+    }
+    else if (carte4.onclick != null) {
+        setTimeout(function() {
+            carte4.classList.remove("border-red-600", "bg-[url(images/drake_en_mode_donuts.webp)]");
+            carte4.classList.add("border-black", "bg-none");
+        }, 600);
+    }
+    if (carte5.onclick == null) {
+        carte5.classList.remove("border-red-600");
+        carte5.classList.add("border-green-600");
+    }
+    else if (carte5.onclick != null) {
+        setTimeout(function() {
+            carte5.classList.remove("border-red-600", "bg-[url(images/finn_jake_hood.webp)]");
+            carte5.classList.add("border-black", "bg-none");
+        }, 600);
+    }
+    if (carte6.onclick == null) {
+        carte6.classList.remove("border-red-600");
+        carte6.classList.add("border-green-600");
+    }
+    else if (carte6.onclick != null) {
+        setTimeout(function() {
+            carte6.classList.remove("border-red-600", "bg-[url(images/finn_jake_hood.webp)]");
+            carte6.classList.add("border-black", "bg-none");
+        }, 600);
+    }
+    if (carte7.onclick == null) {
+        carte7.classList.remove("border-red-600");
+        carte7.classList.add("border-green-600");
+    }
+    else if (carte7.onclick != null) {
+        setTimeout(function() {
+            carte7.classList.remove("border-red-600", "bg-[url(images/mickauto.webp)]");
+            carte7.classList.add("border-black", "bg-none");
+        }, 600);
+    }
+    if (carte8.onclick == null) {
+        carte8.classList.remove("border-red-600");
+        carte8.classList.add("border-green-600");
+    }
+    else if (carte8.onclick != null) {
+        setTimeout(function() {
+            carte8.classList.remove("border-red-600", "bg-[url(images/mickauto.webp)]");
+            carte8.classList.add("border-black", "bg-none");
+        }, 600);
+    }
+
+}
+
 function removeCard(card) {
     pioche1 = 0;
     pioche2 = 0;
     card.onclick = null;
     if (carte1.onclick == null && carte2.onclick == null && carte3.onclick == null && carte4.onclick == null && carte5.onclick == null && carte6.onclick == null && carte7.onclick == null && carte8.onclick == null) {
-        alert('win !');
-        restart();
+        // alert('win !');
+        win.classList.remove('hidden');
+        setTimeout(function() {
+            restart();
+        }, 2000);
     }
 }
 
 function card1() {
+    carte1.classList.remove("border-black", "bg-none");
+    carte1.classList.add("border-red-600", "bg-[url(images/spidermacron.webp)]");
     if (pioche1 == 0) {
         pioche1 = 1;
         carte1.onclick = null;
@@ -91,10 +243,13 @@ function card1() {
         else if (pioche1 == pioche2) {
             removeCard(carte1);
         }
+        nextTurn();
     }
 }
 
 function card2() {
+    carte2.classList.remove("border-black", "bg-none");
+    carte2.classList.add("border-red-600", "bg-[url(images/spidermacron.webp)]");
     if (pioche1 == 0) {
         pioche1 = 1;
         carte2.onclick = null;
@@ -110,10 +265,14 @@ function card2() {
         else if (pioche1 == pioche2) {
             removeCard(carte2);
         }
+        nextTurn();
     }
 }
 
 function card3() {
+    carte3.classList.remove("border-black", "bg-none");
+    carte3.classList.add("border-red-600", "bg-[url(images/drake_en_mode_donuts.webp)]");
+
     if (pioche1 == 0) {
         pioche1 = 2;
         carte3.onclick = null;
@@ -129,10 +288,14 @@ function card3() {
         else if (pioche1 == pioche2) {
             removeCard(carte3);
         }
+        nextTurn();
     }
 }
 
 function card4() {
+    carte4.classList.remove("border-black", "bg-none");
+    carte4.classList.add("border-red-600", "bg-[url(images/drake_en_mode_donuts.webp)]");
+
     if (pioche1 == 0) {
         pioche1 = 2;
         carte4.onclick = null;
@@ -148,10 +311,13 @@ function card4() {
         else if (pioche1 == pioche2) {
             removeCard(carte4);
         }
+        nextTurn();
     }
 }
 
 function card5() {
+    carte5.classList.remove("border-black", "bg-none");
+    carte5.classList.add("border-red-600", "bg-[url(images/finn_jake_hood.webp)]");
     if (pioche1 == 0) {
         pioche1 = 3;
         carte5.onclick = null;
@@ -167,10 +333,13 @@ function card5() {
         else if (pioche1 == pioche2) {
             removeCard(carte5);
         }
+        nextTurn();
     }
 }
 
 function card6() {
+    carte6.classList.remove("border-black", "bg-none");
+    carte6.classList.add("border-red-600", "bg-[url(images/finn_jake_hood.webp)]");
     if (pioche1 == 0) {
         pioche1 = 3;
         carte6.onclick = null;
@@ -186,10 +355,13 @@ function card6() {
         else if (pioche1 == pioche2) {
             removeCard(carte6);
         }
+        nextTurn();
     }
 }
 
 function card7() {
+    carte7.classList.remove("border-black", "bg-none");
+    carte7.classList.add("border-red-600", "bg-[url(images/mickauto.webp)]");
     if (pioche1 == 0) {
         pioche1 = 4;
         carte7.onclick = null;
@@ -205,10 +377,13 @@ function card7() {
         else if (pioche1 == pioche2) {
             removeCard(carte7);
         }
+        nextTurn();
     }
 }
 
 function card8() {
+    carte8.classList.remove("border-black", "bg-none");
+    carte8.classList.add("border-red-600", "bg-[url(images/mickauto.webp)]");
     if (pioche1 == 0) {
         pioche1 = 4;
         carte8.onclick = null
@@ -224,5 +399,6 @@ function card8() {
         else if (pioche1 == pioche2) {
             removeCard(carte8);
         }
+        nextTurn();
     }
 }
